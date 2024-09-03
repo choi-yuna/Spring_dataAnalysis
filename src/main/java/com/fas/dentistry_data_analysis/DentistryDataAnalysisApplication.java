0e829e212bb3c21485bcd3bf6bdb78a4faaaa11e
choi-yuna <mjung0226@naver.com>
2024-09-03 10:11:47 +0900

    initial commit

---
+++ src/main/java/com/fas/dentistry_data_analysis/DentistryDataAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.fas.dentistry_data_analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentistryDataAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentistryDataAnalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fas/dentistry_data_analysis/DentistryDataAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fas.dentistry_data_analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentistryDataAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
