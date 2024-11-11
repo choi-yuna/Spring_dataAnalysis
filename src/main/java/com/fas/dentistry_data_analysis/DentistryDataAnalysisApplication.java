9f200ccba700b3cf476b473583fa95db8db4cac5
DESKTOP-BSAE3OV\fasol <wlsdn4348@naver.com>
2024-11-11 15:22:32 +0900

    [BE-25] :  빌드 변수 추가 및  cors 수정

--- src/main/java/com/fas/dentistry_data_analysis/DentistryDataAnalysisApplication.java
+++ src/main/java/com/fas/dentistry_data_analysis/DentistryDataAnalysisApplication.java
@@ -2,12 +2,18 @@ package com.fas.dentistry_data_analysis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DentistryDataAnalysisApplication {
+public class DentistryDataAnalysisApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DentistryDataAnalysisApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DentistryDataAnalysisApplication.class);
+	}
 }
